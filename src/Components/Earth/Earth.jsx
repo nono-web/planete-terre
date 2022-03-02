e3427cd924d5bf8225ed5dfdf2652597749f8688
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2022-03-02 18:43:52 +0100

    changement thrre.js

--- src/Components/Earth/Earth.jsx
+++ src/Components/Earth/Earth.jsx
@@ -21,8 +21,8 @@ const Earth = (props) => {
   useFrame (({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
 
-    EarthRef.current.rotation.y = elapsedTime / 6;
-    cloudsRef.current.rotation.y = elapsedTime / 6;
+    EarthRef.current.rotation.y = elapsedTime / 5;
+    cloudsRef.current.rotation.y = elapsedTime / 5;
   })
 
   return (
@@ -30,18 +30,14 @@ const Earth = (props) => {
       <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
       <Stars
         radius={300}
-        depth={60}
-        count={20000}
+        count={15000}
         factor={7}
-        saturation={0}
-        fade={true}
       />
          <mesh ref={cloudsRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
           opacity={0.4}
-          depthWrite={true}
           transparent={true}
           side={THREE.DoubleSide}
         />
